ac62e1998b0e51c4151116a15640f28a33fb5c3f
Iurie Guzun <iurie_guzun@yahoo.com>
2020-05-11 22:33:28 -0400

    Modify Color & add accessoryType

--- InspoQuotes/QuoteTableViewController.swift
+++ InspoQuotes/QuoteTableViewController.swift
@@ -50,6 +50,8 @@ class QuoteTableViewController: UITableViewController {
         cell.textLabel?.numberOfLines = 0
         } else {
             cell.textLabel?.text = "Get More Quotest!"
+            cell.textLabel?.textColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            cell.accessoryType = .disclosureIndicator
         }
         
         return cell
